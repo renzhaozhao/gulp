8283be755c7b9c76db6ac84e3894d7d369512d2e
renzhao <3251qq@163.com>
2015-08-11 14:12:44 +0800

    update

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+/*! Tomcat360.com (c) 2015 
+	Author: Renzhao
+*/
+define("index",["log","modules/tabs"],function(e){"use strict";var n=e("log"),o=n.log,s=e("modules/tabs");new s("#reg"),function(){o("欢迎")}()});
---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+define(function(require){
+    "use strict";
+
+    var _ = require('log');
+    var log = _.log;
+
+    var tab = require("modules/tabs");
+    new tab("#reg");
+    
+
+
+    
+    (function(){
+        log('欢迎')
+    })();
+});
